fdf5a647a9bdd18b02abbe0c90c1bdb982797613
Muhammet Mutlu <115688354+mu2tlu@users.noreply.github.com>
2023-06-26 14:43:17 +0300

    Update lcm.c

--- level 3/lcm.c
+++ level 3/lcm.c
@@ -1,5 +1,30 @@
+unsigned int lcm(unsigned int a, unsigned int b)
+{
+	unsigned int n;	
+
+	if (a == 0 || b == 0)
+		return (0);
+	if (a > b)
+		n = a;
+	else
+		n = b;
+	while (1)
+	{
+		if (n % a == 0 && n % b == 0)
+			return (n);
+		++n;
+	}
+}
+
 #include <stdio.h>
-unsigned int hcf(unsigned int a, unsigned int b)
+
+int main(void)
+{
+    printf("%u\n", lcm(6, 7));
+    return 0;
+}
+
+/* unsigned int hcf(unsigned int a, unsigned int b)
 {
     unsigned int hcf = 1;
     unsigned int smallNum = a < b ? a : b;
@@ -20,30 +45,4 @@ unsigned int lcm(unsigned int a, unsigned int b)
     
     unsigned int lcm = (a * b) / hcf(a, b);
     return lcm;
-}
-
-
-int main(void)
-{
-    printf("%u\n", lcm(6, 7));
-    return 0;
-}
-
-
-/* unsigned int lcm(unsigned int a, unsigned int b)
-{
-	unsigned int n;	
-
-	if (a == 0 || b == 0)
-		return (0);
-	if (a > b)
-		n = a;
-	else
-		n = b;
-	while (1)
-	{
-		if (n % a == 0 && n % b == 0)
-			return (n);
-		++n;
-	}
 } */
