7328309361d153d2b6ccdcf6a906e6399214924a
Muhammet Mutlu <115688354+mu2tlu@users.noreply.github.com>
2023-06-26 14:43:13 +0300

    Update pgcd.c

--- level 3/pgcd.c
+++ level 3/pgcd.c
@@ -3,6 +3,30 @@
 
 int main(int ac, char **av)
 {
+    int s1 = atoi(av[1]);
+    int s2 = atoi(av[2]);
+    if(ac == 3)
+    {
+        if(s1 > 0 && s2 > 0)
+        {
+            while(s1 != s2)
+            {
+                if(s1 > s2)
+                    s1 -= s2;
+                else
+                    s2 -= s1;
+            }
+            printf("%d", s1);
+        }
+    }
+    printf("\n");
+}
+
+/* #include <stdio.h>
+#include <stdlib.h>
+
+int main(int ac, char **av)
+{
     int flag = 0;
     int i = 1;
     int a = atoi(av[1]);
@@ -20,29 +44,6 @@ int main(int ac, char **av)
         } 
         if(i - 1 == num)
             printf("%d", flag);
-    }
-    printf("\n");
-}
-
-/* #include <stdio.h>
-#include <stdlib.h>
-int main(int ac, char **av)
-{
-    int s1 = atoi(av[1]);
-    int s2 = atoi(av[2]);
-    if(ac == 3)
-    {
-        if(s1 > 0 && s2 > 0)
-        {
-            while(s1 != s2)
-            {
-                if(s1 > s2)
-                    s1 -= s2;
-                else
-                    s2 -= s1;
-            }
-            printf("%d", s1);
-        }
     }
     printf("\n");
 } */
