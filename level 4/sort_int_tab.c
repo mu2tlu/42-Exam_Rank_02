9efad9b314fa1a99cdb6ab72d8fedc48b8adc512
Muhammet Mutlu <115688354+mu2tlu@users.noreply.github.com>
2023-07-07 11:57:06 +0300

    Update sort_int_tab.c

--- level 4/sort_int_tab.c
+++ level 4/sort_int_tab.c
@@ -8,7 +8,7 @@ void	sort_int_tab(int *tab, unsigned int size)
 		j = i;
 		while (j < size)
 		{
-			if (tab[i] > tab[j])
+			if (tab[i] > tab[j] && tab[i] != tab[j])
 			{
 				temp = tab[i];
 				tab[i] = tab[j];
@@ -24,8 +24,8 @@ void	sort_int_tab(int *tab, unsigned int size)
 
 int	main(void)
 {
-	unsigned int	i = 0;
-	unsigned int	tab[] = {0, 4, 1, 3, 2};
+	unsigned int i = 0;
+	int tab[] = {0, 4, 1, 3, 2};
 	sort_int_tab(tab, 5);
 	while (i < 5)
 	{
