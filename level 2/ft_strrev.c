cb5dbc3258d4336a91397e6ba7b947d25167646e
Muhammet Mutlu <115688354+mu2tlu@users.noreply.github.com>
2023-07-07 12:01:50 +0300

    Update ft_strrev.c

--- level 2/ft_strrev.c
+++ level 2/ft_strrev.c
@@ -1,21 +1,18 @@
 #include <stdio.h>
 
-char    *ft_strrev(char *str)
+char	*ft_strrev(char *str)
 {
-    int i = 0;
-    int len = 0;
-    char temp;
-    while (str[len] != '\0')
-        len++;
-    len--;
-    while (len > i)
-    {
-        temp = str[i];
-        str[i] = str[len];
-        str[len] = temp;
-        len--;
-        i++;
-    }
-    return str;
+	int		i = 0, len = 0;
+	char	temp;
 
+	while (str[len] != '\0')
+		len++;
+	while (--len > i)
+	{
+		temp = str[i];
+		str[i] = str[len];
+		str[len] = temp;
+		i++;
+	}
+	return (str);
 }
