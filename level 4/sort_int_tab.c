376937301eda86de5c362779edc594ce43809e41
Muhammet Mutlu <115688354+mu2tlu@users.noreply.github.com>
2023-07-13 14:55:11 +0300

    Update sort_int_tab.c

--- level 4/sort_int_tab.c
+++ level 4/sort_int_tab.c
@@ -33,3 +33,24 @@ int	main(void)
 		i++;
 	}
 }
+
+
+/* void	sort_int_tab(int *tab, unsigned int size)
+{
+	unsigned int i;
+	int tmp;
+
+	i = 0;
+	while (i < (size - 1))
+	{
+		if (tab[i] > tab[i + 1])
+		{
+			tmp = tab[i];
+			tab[i] = tab[i + 1];
+			tab[i + 1] = tmp;
+			i = 0;
+		}
+		else
+			i++;
+	}
+} */
