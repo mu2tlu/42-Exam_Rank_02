248ab440607fb05d0db789acec5ec519783f8a05
Muhammet Mutlu <115688354+mu2tlu@users.noreply.github.com>
2023-07-14 23:24:25 +0300

    Update lcm.c

--- level 3/lcm.c
+++ level 3/lcm.c
@@ -23,26 +23,3 @@ int main(void)
     printf("%u\n", lcm(6, 7));
     return 0;
 }
-
-/* unsigned int hcf(unsigned int a, unsigned int b)
-{
-    unsigned int hcf = 1;
-    unsigned int smallNum = a < b ? a : b;
-    unsigned int i = 1;
-    while (i <= smallNum)
-    {
-        if (a % i == 0 && b % i == 0)
-            hcf = i;
-        i++;
-    }
-    return hcf;
-}
-
-unsigned int lcm(unsigned int a, unsigned int b)
-{
-    if (a == 0 || b == 0)
-        return 0;
-    
-    unsigned int lcm = (a * b) / hcf(a, b);
-    return lcm;
-} */
