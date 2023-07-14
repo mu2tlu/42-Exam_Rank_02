3816b2681eff1d0a666ff77ee334e9151fdbd17b
Muhammet Mutlu <115688354+mu2tlu@users.noreply.github.com>
2023-07-14 23:46:47 +0300

    Update last_word_2.c

--- level 2/last_word/last_word_2.c
+++ level 2/last_word/last_word_2.c
@@ -8,11 +8,11 @@ int	main(int ac, char **av)
 	{
 		while(av[1][i])
 		{
-			if((av[1][i] < 33 && av[1][i]) && (av[1][i + 1] > 32 && av[1][i + 1] < 127))
+			if((av[1][i] == 32 || (av[1][i] >= 9 && av[1][i] <= 13)) && (av[1][i + 1] > 32 && av[1][i + 1] < 127) && av[1][i])
 				j = i + 1;
 				i++;
 		}
-		while(av[1][j] > 32 && av[1][j] < 127)
+		while((av[1][j] > 32 && av[1][j] < 127) &&av[1][i])
 		{
 			write(1, &av[1][j], 1);
 			j++;
