5d58485fe2522b20976f9fde815c9810e32c7fc4
Muhammet Mutlu <115688354+mu2tlu@users.noreply.github.com>
2023-07-16 17:26:07 +0300

    Update ft_split.c

--- level 4/ft_split.c
+++ level 4/ft_split.c
@@ -1,50 +1,46 @@
 #include <stdlib.h>
 
-char    **ft_split(char *str)
+char	**ft_split(char *str)
 {
-    int i = 0, a = 0, b = 0, len = 0;
+	int		i = 0, a = 0, b = 0, len = 0;
 
-    while (str[++len]);
-
-    char **split = (char **)malloc(sizeof(char *) * len);
-
-    if(!split)
-        return 0;
-
-    while (str[i])
-    {
-        if(str[i] > 32 && str[i])
-        {
-            split[a] = (char *)malloc(sizeof(char) * len);
-            
-            b = 0;    
-            
-            while (str[i] > 32 && str[i])
-            {
-                split[a][b] = str[i];
-                b++;
-                i++;
-            }
-            split[a][b] = '\0';
-            a++;
-        }
-        else
-            i++;
-    }
-    split[a] = 0;
-    return split;
+	while (str[len])
+		len++;
+	char	**split = (char **)malloc(sizeof(char *) * len);
+	if (!split)
+		return (0);
+	while (str[i])
+	{
+		if (str[i] > 32 && str[i])
+		{
+			split[a] = (char *)malloc(sizeof(char) * len);
+			b = 0;
+			while (str[i] > 32 && str[i])
+			{
+				split[a][b] = str[i];
+				b++;
+				i++;
+			}
+			split[a][b] = '\0';
+			a++;
+		}
+		else
+			i++;
+	}
+	split[a] = 0;
+	return (split);
 }
 
-
 #include <stdio.h>
-int main()
+
+int	main(void)
 {
-    char *str = "muhammet mutlu";
-    char **av = ft_split(str);
-    int i = 0;
-    for(i; av[i] != 0;)
-    {
-        printf("%s\n", av[i]);
-        i++;
-    }
+	char *str = "muhammet mutlu";
+	char **av = ft_split(str);
+
+	for (int i = 0; av[i] != 0;)
+	{
+		printf("%s\n", av[i]);
+		i++;
+	}
 }
