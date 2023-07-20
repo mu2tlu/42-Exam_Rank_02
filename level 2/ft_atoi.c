0acfde83d76483c43d1ba03754b2b95008dca943
Muhammet Mutlu <115688354+mu2tlu@users.noreply.github.com>
2023-07-20 18:20:04 +0300

    Update ft_atoi.c

--- level 2/ft_atoi.c
+++ level 2/ft_atoi.c
@@ -1,19 +1,38 @@
 int	ft_atoi(const char *str)
 {
-    int i = 0;
-    int sign = 1;
-    int result  = 0;
+	int	i;
+	int	sign;
+	int	result;
 
-    while (str[i] == 32 || (str[i] >= 9 && str[i] <= 13))
-        i++;
-    if (str[i] == '-')
-            sign = -1;
-    while (str[i] == '+' || str[i] == '-')
-        i++;
-    while (str[i] && (str[i] >= '0' && str[i] <= '9'))
-    {
-        result  = (result  * 10) + (str[i] - '0');
-        i++;
-    }
-    return(result * sign);
+	i = 0;
+	sign = 1;
+	result = 0;
+	while (str[i] == 32 || (str[i] >= 9 && str[i] <= 13))
+		i++;
+	if (str[i] == '-')
+	{
+		sign = -1;
+		i++;
+		if (str[i] == '-' || str[i] == '+')
+			return (0);
+	}
+	else if (str[i] == '+')
+	{
+		i++;
+		if (str[i] == '-' || str[i] == '+')
+			return (0);
+	}
+	while (str[i] && (str[i] >= '0' && str[i] <= '9'))
+	{
+		result = (result * 10) + (str[i] - '0');
+		i++;
+	}
+	return (result * sign);
+}
+
+#include <stdio.h>
+
+int	main(void)
+{
+	printf("%d", ft_atoi("             +1234 asd  231"));
 }
