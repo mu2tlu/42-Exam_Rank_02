fb3f46c92062052c1101ea50c17aa9f3cd8889a1
Muhammet Mutlu <115688354+mu2tlu@users.noreply.github.com>
2023-08-03 23:36:23 +0300

    Update rev_wstr.c

--- level 4/rev_wstr.c
+++ level 4/rev_wstr.c
@@ -11,44 +11,35 @@ void	ft_putstr(char *str)
 	}
 }
 
-char    **ft_split(char *str)
+char	**ft_split(char *str)
 {
-    int i = 0, a = 0, b = 0, len = 0;
+	int		i = 0, a = 0, b = 0, len = 0;
 
-    while (str[++len]);
-
-    char **split = (char **)malloc(sizeof(char *) * len);
-
-    if(!split)
-        return 0;
-
-    while (str[i])
-    {
-        if(str[i] > 32 && str[i])
-        {
-            split[a] = (char *)malloc(sizeof(char) * len);
-            
-            b = 0;    
-            
-            while (str[i] > 32 && str[i])
-            {
-                split[a][b] = str[i];
-                b++;
-                i++;
-            }
-            split[a][b] = '\0';
-            a++;
-        }
-        else
-            i++;
-    }
-    if (a == 0) // Sadece boşluk karakterleri varsa
-    {
-        free(split);
-        return 0;
-    }
-    split[a] = 0;
-    return split;
+	while (str[len])
+		len++;
+	char	**split = (char **)malloc(sizeof(char *) * len);
+	if (!split)
+		return (0);
+	while (str[i])
+	{
+		if (str[i] > 32 && str[i])
+		{
+			split[a] = (char *)malloc(sizeof(char) * len);
+			b = 0;
+			while (str[i] > 32 && str[i])
+			{
+				split[a][b] = str[i];
+				b++;
+				i++;
+			}
+			split[a][b] = '\0';
+			a++;
+		}
+		else
+			i++;
+	}
+	split[a] = 0;
+	return (split);
 }
 
 int main(int ac, char **av)
